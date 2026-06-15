6b22c1bfa2d0e64174186a5171c0666f2fcb8c4d
Valery Blagodarov <11w-walerik@mail.ru>
2026-06-15 17:38:39 +0300

    26 done

---
+++ src/main/java/r2_multithreading/Test26.java
@@ -0,0 +1,53 @@
+package main.java.r2_multithreading;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Test26 {
+    public static void main(String[] args) throws InterruptedException {
+        Task task = new Task();
+
+        Thread thread1 = new Thread(task::firstThread);     // упрощение лямбды - ссылка на метод
+        Thread thread2 = new Thread(task::secondThread);    // task::firstThread - объект::метод
+
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+
+        task.showCounter();
+    }
+}
+
+
+class Task {
+    private int counter;
+    private final Lock lock = new ReentrantLock();
+
+    private void increment() {
+        for (int i = 0; i < 10000; i++) {
+            counter++;
+        }
+    }
+
+    public void firstThread() {
+        lock.lock();
+        increment();
+        lock.unlock();
+    }
+
+    public void secondThread() {
+        lock.lock();
+        increment();
+        lock.unlock();
+    }
+
+    public void showCounter() {
+        System.out.println(counter);
+    }
+}
+
+
+
+
